ceb0e86764eb87f14cc3f1cbc812d14b224e291d
Wendy <171596327@qq.com>
2025-06-20 13:39:31 +0800

    增加跳转知识图谱页面

--- ZTF-VUE/src/components/WebHeader.vue
+++ ZTF-VUE/src/components/WebHeader.vue
@@ -11,9 +11,15 @@
         <router-link to="/" class="text-gray-600 hover:text-primary transition-colors">认识邹韬奋</router-link>
         <router-link to="/" class="text-gray-600 hover:text-primary transition-colors">漫画邹韬奋</router-link>
         <router-link to="/" class="text-gray-600 hover:text-primary transition-colors">韬奋时评</router-link>
-        <router-link to="/" class="text-gray-600 hover:text-primary transition-colors">韬奋知识图谱</router-link>
+        <router-link
+          to="/knowledge-graph"
+          class="text-gray-600 hover:text-primary transition-colors"
+          :class="{ 'text-primary': $route.path === '/knowledge-graph' }"
+        >
+          韬奋知识图谱
+        </router-link>
       </nav>
-      </div>                        
+      </div>
       <button class="md:hidden text-gray-600" @click="toggleMobileMenu">
         <i class="fas fa-bars text-xl"></i>
       </button>
---
+++ ZTF-VUE/src/views/KnowledgeGraph.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <WebHeader />
+    <div class="flex-1 overflow-hidden">
+      <iframe
+        src="/vue_project/chat_interface_interactive.html"
+        class="w-full h-full border-none"
+        style="transform: scale(1); transform-origin: 0 0;"
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import WebHeader from '@/components/WebHeader.vue'
+</script>
+
+<style scoped>
+iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+  overflow: hidden;
+}
+</style>
